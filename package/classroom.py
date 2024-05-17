3d8b05e7551a5e785c7c4c311d68e40725575d29
Sanawar_21 <sanawarsaeed@gmail.com>
2024-05-18 02:45:10 +0500

    add classroom class

--- package/classroom.py
+++ package/classroom.py
@@ -1,75 +1,144 @@
-import os.path
 import json
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-SCOPES = ["https://www.googleapis.com/auth/classroom.courses.readonly"]
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from client import BaseClient
 
 
-class Classroom:
-    # TODO: separate class for gmail, classroom and sheets
-    pass
+class Student:
+
+    def __init__(self, student) -> None:
+        try:
+            self.course_id = student["courseId"]
+        except KeyError:
+            self.course_id = None
+        try:
+            self.full_name = student["profile"]["name"]["fullName"]
+        except KeyError:
+            self.full_name = None
+
+    def as_dict(self):
+        return {
+            "course_id": self.course_id,
+            "full_name": self.full_name
+        }
 
 
 class Course:
-    def __init__(self, course, meet_link=None) -> None:
+    def __init__(self, course) -> None:
         self.name = course["name"]
+        self.id = course["id"]
         self.link = course["alternateLink"]
         try:
             self.description = course["description"]
         except KeyError:
-            print(course)
             self.description = None
         try:
             self.room = course["room"]
         except KeyError:
             self.room = None
-        self.meet_link = meet_link
+        self.meet_link = self.room
+        self.time_created = course["creationTime"]
 
     def as_dict(self):
         course_dict = {
+            "id": self.id,
             "name": self.name,
             "link": self.link,
             "description": self.description,
             "meet_link": self.meet_link,
-            "room": self.room,
+            "time_created": self.time_created,
         }
         return course_dict
 
 
-creds = None
-if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            "data/secret.json", SCOPES
+class Classroom(BaseClient):
+
+    def __init__(
+            self,
+            client_secret_path="data/secret.json",
+            credentials_path="data/credentials.json",
+            courses_path="data/courses.json",
+            force_renew=False
+    ) -> None:
+        super().__init__(
+            "classroom",
+            "v1",
+            client_secret_path,
+            credentials_path,
+            courses_path,
+            [
+                "https://www.googleapis.com/auth/classroom.courses.readonly",
+                "https://www.googleapis.com/auth/classroom.rosters.readonly",
+            ],
+            force_renew
         )
-        creds = flow.run_local_server(port=0)
 
-    with open("token.json", "w") as token:
-        token.write(creds.to_json())
+    @ staticmethod
+    def is_older_than_one_month(datetime_str):
+        input_datetime = datetime.fromisoformat(datetime_str[:-1])
+        current_datetime = datetime.utcnow()
+        one_month_ago = current_datetime - relativedelta(months=1)
+        return input_datetime < one_month_ago
 
-service = build("classroom", "v1", credentials=creds)
-results = service.courses().list(pageSize=10, pageToken=None).execute()
-next_token = results.get("nextPageToken")
-courses = results.get("courses", [])
-if not courses:
-    print("No courses found.")
-print("Courses:")
-for course in courses:
-    print(course["name"])
+    def get_courses(self):
+        courses: list[Course] = []
+        results = self.service.courses().list(pageSize=10, pageToken=None).execute()
+        next_token = results.get("nextPageToken")
+        for course in results.get("courses", []):
+            courses.append(Course(course))
 
-course_dicts = []
-results = service.courses().list(pageSize=10).execute()
-pageToken = results.get("nextPageToken")
-courses = results.get("courses", [])
-while pageToken:
-    results = service.courses().list(pageSize=10, pageToken=pageToken).execute()
-    courses = results.get("courses", [])
-    for course in courses:
-        course_dicts.append(Course(course).as_dict())
-    pageToken = results.get("nextPageToken")
+        if courses:
+            while next_token and not self.is_older_than_one_month(courses[-1].time_created):
+                results = self.service.courses().list(
+                    pageSize=10, pageToken=next_token).execute()
+                next_token = results.get("nextPageToken")
+                for course in results.get("courses", []):
+                    courses.append(Course(course))
+
+        return courses
+
+    def get_students(self, courseId):
+        # implement and save in new data.json and ask in init whether or reload the db or not
+        students: list[Student] = []
+        results = self.service.courses().students().list(
+            courseId=courseId, pageSize=10, pageToken=None).execute()
+        next_token = results.get("nextPageToken")
+        for student in results.get("students", []):
+            students.append(Student(student))
+
+        if students:
+            while next_token:
+                results = self.service.courses().students().list(
+                    courseId=courseId, pageSize=10, pageToken=next_token).execute()
+                next_token = results.get("nextPageToken")
+                for student in results.get("students", []):
+                    students.append(Student(student))
+
+        return students
+
+    def save(self, iterable: list[Student | Course]):
+        if iterable:
+            if type(iterable[0]) == Student:
+                data = {iterable[0].course_id: [
+                    student.full_name for student in iterable]}
+                path = "data/students.json"
+            elif type(iterable[0]) == Course:
+                data = {course.id: course.as_dict() for course in iterable}
+                path = "data/courses.json"
+
+            try:
+                with open(path) as file:
+                    old_data = json.loads(file.read())
+            except (FileNotFoundError, json.decoder.JSONDecodeError):
+                old_data = {}
+
+            with open(path, "w") as file:
+                old_data.update(data)
+                file.write(json.dumps(old_data))
+
+    def generate_database(self):
+        courses = self.get_courses()
+        self.save(courses)
+        for course in courses:
+            students = self.get_students(course.id)
+            self.save(students)
