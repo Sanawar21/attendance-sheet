46ed1229c6608d8d80c70b50fef0168c0b1a08ad
Sanawar_21 <sanawarsaeed@gmail.com>
2024-05-18 22:51:10 +0500

    improve logging

--- main.py
+++ main.py
@@ -23,8 +23,11 @@ def get_env():
 
 if __name__ == "__main__":
     print("Welcome To Attendence Sheet Bot")
+
     force_renew = input(
-        "Forcefully renew the API tokens? (y/n) ").lower() == "y"
+        "Forcefully renew the API tokens? (y/n) "
+    ).lower() == "y"
+
     env = get_env()
 
     username = env["SALESFORCE_USERNAME"]
@@ -34,17 +37,35 @@ if __name__ == "__main__":
     gc = GmailClient(force_renew=force_renew)
     cc = ClassroomClient(force_renew=force_renew)
     ac = AttendanceClient(username, password, security_token)
+
+    regen_db = input("Do you wish to regenerate the students and courses database? (Not recommended; takes a lot of time; irreversible; cannot stop before completion) (y/n) ").lower() == "y"
+
+    if regen_db:
+        cc.classes_start_date = input(
+            "Enter the minimum creation date of the database: (dd/mm/yyyy) "
+        )
+
+        if input("Do you wish to continue? (y/n)").lower() == "y":
+            print("Generating the database...")
+            cc.generate_database()
+            print("Database regenerated successfully.")
+            quit()
+
+    print("Getting attendees...")
     attendees = gc.get_attendees()
+    print("Getting absentees...")
     absentees = cc.get_absentees(attendees)
+
+    print("Uploading to salesforce...")
 
     for attendee in attendees:
         ac.upload(attendee)
         print(
-            f"Uploaded attendance for {attendee["Last name"]} (Course: {attendee['Course']})"
+            f'Uploaded attendance for {attendee["Last name"]} (Course: {attendee["Course"]})'
         )
 
     for absentee in absentees:
-        ac.upload(absentee)
+        ac.upload(absentee, is_absentee=True)
         print(
-            f"Uploaded absence for {absentee["Last name"]} (Course: {absentee['Course']})"
+            f'Uploaded absence for {absentee["Last name"]} (Course: {absentee["Course"]})'
         )
